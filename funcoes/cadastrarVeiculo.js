d2ada3182a30b1f05ac6060a947ea9b2bfb2a86b
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2024-09-05 23:23:12 -0300

    feat: funcao de cadastro de veiculos implementadas.

---
+++ funcoes/cadastrarVeiculo.js
@@ -0,0 +1,35 @@
+import PromptSync from "prompt-sync";
+import { Carro } from "../classes/Carro.js";
+import { Motocicleta } from "../classes/Motocicleta.js";
+
+let prompt = new PromptSync();
+const listaCarros = [];
+const listaMotos = [];
+
+export function cadastrarCarro() {
+    let marca = prompt('Informe a marca: ');
+    let placa = prompt('Informe o número da placa: ');
+    let combustivel = prompt('Informe o tipo de combustivel: ');
+    let ano = +prompt('Informe o ano de lançamento: ');
+    let cor = prompt('Informe a cor do veículo: ');
+    let portas = prompt('Informe a quantidade de portas: ');
+    let cambio = prompt('Informe o tipo de cambio: ');
+
+    const car = new Carro(marca, placa, combustivel, ano, cor, portas, cambio);
+    listaCarros.push(car);
+    return listaCarros;
+}
+
+export function cadastrarMoto() {
+    let marca = prompt('Informe a marca: ');
+    let placa = prompt('Informe o número da placa: ');
+    let combustivel = prompt('Informe o tipo de combustivel: ');
+    let ano = +prompt('Informe o ano de lançamento: ');
+    let cor = prompt('Informe a cor da moto: ');
+    let cilindradas = prompt('Informe a quantidade de cilindradas: ');
+    let tipo = prompt('Informe o tipo de motocicleta: ');
+
+    const moto = new Motocicleta(marca, placa, combustivel, ano, cor, cilindradas, tipo);
+    listaMotos.push(moto);
+    return listaMotos;
+}
--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-import { Carro } from "./classes/Carro.js";
-import { Motocicleta } from "./classes/Motocicleta.js";
+import PromptSync from "prompt-sync";
+import { cadastrarCarro, cadastrarMoto } from "./funcoes/cadastrarVeiculo.js";
 
 
-const car = new Carro('Fiat', '1234AA', 'Gasolina', 2024, 'Azul', 4, 'Auto');
 
-console.log(car.info());
+let prompt = new PromptSync();
 
-const moto = new Motocicleta('Honda', '123DD', 'Gasolina', 2020, 'Veremelho', 100, 'Scooter');
-console.log(moto.info());
+let opcao = +prompt('Digite 1: Cadastrar Carro | Digite 2: Cadastrar Moto: ');
+let selecaoCadastro = opcao == 1 ? '**** Cadastro de Carro ****' : '**** Cadastro de Moto ****';
+
+
+switch (opcao) {
+    case 1:
+        console.log('\n');
+        console.log(selecaoCadastro);
+        console.log('\n');
+        cadastrarCarro();
+        break;
+
+    case 2:
+        console.log('\n');
+        console.log(selecaoCadastro);
+        console.log('\n');
+        cadastrarMoto();
+        break;
+
+    case 3:
+        console.log('Aplicação Finalizada!');
+        break;
+
+    default:
+        console.log('Opção Inválida!');
+        break;
+}
+
+
+
+// const listaVeiculos = [...listaCarros, ...listaMotos];
+// console.log(listaVeiculos);
